4f456830e6d00de99c797d060438662e37ad7d96
thalessanches123 <thales.sanches174@gmail.com>
2021-06-28 18:48:01 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,17 @@
 package application;
 
+import java.util.Scanner;
+
+import model.entities.Department;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		Department obj = new Department(1, "books");
+		
+		System.out.println(obj);
 
 	}
 
