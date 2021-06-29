e50bcda529397d7bd045c8bdde18b5c70ac91779
thalessanches123 <thales.sanches174@gmail.com>
2021-06-29 11:51:17 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -43,6 +43,11 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("insertednew id : " + newSeller.getId());
 		
+		System.out.println("\n=== TEST5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Eine");
+		sellerDao.update(seller);
+		System.out.println("updated completo");
 		
 	}
 
